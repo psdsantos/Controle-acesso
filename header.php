b52a26a3bc2e92874323e570d0a821d8bcb8f079
sdsPedro <pedro.santos061@academico.ifs.edu.br>
2020-07-31 12:04:17 -0300

    Inicio primeiras paginas

---
+++ header.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+    <title>titulo</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-5">
+        <a class="navbar-brand pr-5" href="index.php">Ta funfando</a>
+        
+        <div id="navbar">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="./turma.php">link 1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">link 2</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include('header.php');?>
+    <div class="container">
+        <h3>um dois três</h3>
+    </div>
+
+</body>
+
+</html>
