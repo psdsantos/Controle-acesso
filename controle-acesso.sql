3f5cad65c2a15328d872f18239dc9f358a24851d
Pedro Silva dos Santos <pedroslvsantos@gmail.com>
2020-11-17 21:04:35 -0300

    Update controle-acesso.sql

--- controle-acesso.sql
+++ controle-acesso.sql
@@ -124,4 +124,5 @@ INDEX Registro_Acesso_FKIndex2(Autorizacao_cod_autorizacao),
 -- PASSWORD: 465132
 INSERT INTO `coordenacao` (`Cod_coordenacao`, `Nome`, `Sigla`) VALUES ('1', 'Coordenadoria de informática', 'COINF');
 INSERT INTO `turma` (`Cod_turma`, `Coordenacao_cod_coordenacao`, `Nome`, `Sigla`) VALUES (1, 1, '3° ano integrado em informática', '3IINF');
+INSERT INTO `categoria` (`Cod_categoria`, `Descricao`) VALUES ('1', 'Coordenação');
 INSERT INTO `usuario` (`matricula`, `Categoria_cod_categoria`, `Coordenacao_cod_coordenacao`, `Nome`, `Rfid`, `Senha`, `Status_usuario`) VALUES ('321654987', '1', '1', 'Pedro Silva dos Santos', '0', '465132', '1');
